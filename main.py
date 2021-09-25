ef140bb3b7cfb05807a42c166a7ff0791afed0df
Karina Singatullina <69847727+curlykorine@users.noreply.github.com>
2021-09-26 01:40:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,26 +14,28 @@ while True:
     if command[0].lower() == "add":
         user.add_to_check_list(command[1])
 
-    #ex: del https://www.google.ru/
-    elif command[0].lower() == "del":
+    #ex: delete https://www.google.ru/
+    elif command[0].lower() == "delete":
         user.del_from_check_list(command[1])
 
-    #ex: update_add https://docs.google.com/ - site should be in checklist
-    elif command[0].lower() == "update_add":
+    #ex: on https://docs.google.com/ - site should be in checklist
+    elif command[0].lower() == "on":
         try:
             user.update_to_check(command[1])
         except:
             print("site not in checklist")
 
-    # ex: update_remove https://docs.google.com/ - site should be in checklist
-    elif command[0].lower() == "update_remove":
+    # ex: off https://docs.google.com/ - site should be in checklist
+    elif command[0].lower() == "off":
         try:
             user.update_not_to_check(command[1], user.conn, user.cursor)
         except:
             print("site not in checklist")
 
+    # ex: checklist        
     elif command[0].lower() == "checklist":
         print("List of sites:",user.get_check_list(user.cursor))
-    #just quit
+        
+    # ex: quit
     elif command[0].lower() == "quit":
         break
