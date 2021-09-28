733bafbab092b99c5398f24d53963099b47bcc4a
Karina Singatullina <69847727+curlykorine@users.noreply.github.com>
2021-09-28 21:06:25 +0300

    Quit command update

---
+++ main.py
@@ -0,0 +1,48 @@
+from checker.Pinger import Pinger
+from checker.WindowsClient import Windows_Client
+from threading import Thread
+
+user = Windows_Client()
+service = Pinger()
+service.register(user)
+thread = Thread(target= service.run)
+thread.start()
+
+try:
+    while True:
+        command = input().split()
+        #ex: add https://yandex.ru/
+        if command[0].lower() == "add":
+            user.add_to_check_list(command[1])
+
+        #ex: del https://www.google.ru/
+        elif command[0].lower() == "del":
+            user.del_from_check_list(command[1])
+
+        #ex: update_add https://docs.google.com/ - site should be in checklist
+        elif command[0].lower() == "update_add":
+            try:
+                user.update_to_check(command[1])
+            except:
+                print("site not in checklist")
+
+        # ex: update_remove https://docs.google.com/ - site should be in checklist
+        elif command[0].lower() == "update_remove":
+            try:
+                user.update_not_to_check(command[1])
+            except:
+                print("site not in checklist")
+
+        elif command[0].lower() == "checklist":
+            print("List of sites:",user.get_check_list())
+
+        #just quit
+        elif command[0].lower() == "quit":
+            service._to_check = False
+            thread.join()
+            break
+
+except Exception as e:
+    print(e)
+finally:
+    quit()
